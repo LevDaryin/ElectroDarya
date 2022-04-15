644f7b072d6785f794ed1b3279b819a4fe20e09a
Lev <rumataeston@gmail.com>
2022-04-15 15:54:19 +0300

    cite, first version

---
+++ dist/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: false,
+                }
+            },
+        ]
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400 && $(this).scrollTop() < 3800) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.promo__navigation'),
+        menuItem = document.querySelectorAll('.promo__link'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('promo__navigation_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('promo__navigation_active');
+            })
+        })
+    })
+});
